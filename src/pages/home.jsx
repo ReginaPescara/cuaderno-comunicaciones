5dcc3ded4be3d5add56c78775710b7aee2b41e96
Regina Pescara <regina.pescara.epet20@gmail.com>
2023-05-09 18:11:33 -0300

    Importando componente burger

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,5 @@
 import Boton from './componentes/inputHome.jsx'
+import Burger from './componentes/burger.jsx'
 function Home (){
     return(
     <div className='contenedor'><><Boton texto="Comunicaciones generales" /><Boton texto="Retiros" /></>
