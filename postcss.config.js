cb111fe042646d5e98b387ae7cbbc86f75046750
GiulianoDuran <durangiulianoepet20@gmail.com>
2023-05-09 18:19:56 -0300

    modificando propiedad plugin

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,8 @@ const autoprefixer = require('autoprefixer');
 
 module.exports = {
     plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
+      tailwindcss,
+      autoprefixer,
     ],
 }
 
