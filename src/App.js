dea78334df9a71d9ab285f34613bab8f2150ce76
GiulianoDuran <durangiulianoepet20@gmail.com>
2023-05-30 20:43:08 -0300

    Eliminando duplicados

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import Avisos from './componentes/Avisos';
 
 function App() {
@@ -23,13 +22,14 @@ function App() {
       </header>
       <Avisos></Avisos>
     </div>
-=======
+  )
+}
 import Home from './pages/home.jsx'
 
 function App() {
   return (
     <Home/>
->>>>>>> 16885f4a4f18c65527c7d203e59466d1905dd090
+    
   );
 }
 
