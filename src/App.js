fe8046829c9966f4bdd23eb97207146612f831e4
lucascardenas7 <lucas13rojo@gmail.com>
2023-09-13 11:26:55 -0300

    Creando el Registro y Menu Principal de la APP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Contactos from './Componentes/Contactos';
-import Avisos from './Componentes/Avisos';
 
 function App(){
   return(
--- src/Componentes/Contactos.jsx
+++ src/Componentes/Contactos.jsx
@@ -38,16 +38,13 @@ export default function Example() {
         />
       </div>
       <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
-        </p>
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Registrarse</h2>
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
-              First name
+              Nombre
             </label>
             <div className="mt-2.5">
               <input
@@ -61,7 +58,7 @@ export default function Example() {
           </div>
           <div>
             <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
-              Last name
+              Apellido
             </label>
             <div className="mt-2.5">
               <input
@@ -89,7 +86,7 @@ export default function Example() {
           </div>
           <div>
             <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
-              Password
+              Contraseña
             </label>
             <div className="mt-2.5">
               <input
@@ -103,7 +100,7 @@ export default function Example() {
           </div>
           <div>
             <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
-              Confirm Password
+              Confirmar Contraseña
             </label>
             <div className="mt-2.5">
               <input
@@ -115,35 +112,17 @@ export default function Example() {
               />
             </div>
           </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
-              Phone number
+          <div>
+            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+              Numero de Telefono
             </label>
-            <div className="relative mt-2.5">
-              <div className="absolute inset-y-0 left-0 flex items-center">
-                <label htmlFor="country" className="sr-only">
-                  Country
-                </label>
-                <select
-                  id="country"
-                  name="country"
-                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-                >
-                  <option>ARG</option>
-                  <option>CHI</option>
-                  <option>URU</option>
-                </select>
-                <ChevronDownIcon
-                  className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </div>
+            <div className="mt-2.5">
               <input
-                type="tel"
-                name="phone-number"
-                id="phone-number"
-                autoComplete="tel"
-                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                type="number"
+                name="confirm"
+                id="confirm"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
@@ -170,7 +149,7 @@ export default function Example() {
             <Switch.Label className="text-sm leading-6 text-gray-600">
               Desea guardar la informacion del registro?{' '}
               <a href="#" className="font-semibold text-indigo-600">
-                privacy&nbsp;policy
+                Privacy&nbsp;Policy
               </a>
               .
             </Switch.Label>
@@ -181,7 +160,7 @@ export default function Example() {
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Let's talk
+            Siguiente
           </button>
         </div>
       </form>
