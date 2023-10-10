08b5cdedea6faf4dfbef3f74f44aa416aa373990
lucascardenas7 <lucas13rojo@gmail.com>
2023-10-10 11:06:05 -0300

    Agregando codigo en RegistrarsePreceptores

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import RegistrarseAlumno from './Componentes/Registros/RegistrarseAlumno';
 import RegistrarsePadres from './Componentes/Registros/RegistrarsePadres';
+import RegistrarsePreceptor from './Componentes/Registros/RegistrasePreceptor';
 
 function App(){
   return(
-    <RegistrarsePadres></RegistrarsePadres>
+    <RegistrarsePreceptor></RegistrarsePreceptor>
   )
   }
 export default App;
