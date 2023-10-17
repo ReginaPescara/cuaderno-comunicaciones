0b4297211e8d0a28705926cc1d64a0b7e1bc36ce
alejo <alejonoviello@gmail.com>
2023-10-17 19:43:47 -0300

    Arreglando errores del app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-
-import Signin from "./pages/Signin";
-import Registrarse from './Componentes/Registrarse'
-import FondoDePantalla from './Componentes/FondoPantalla';
-
-function App(){
-  return(
-    
-      <Signin></Signin>
-   
-=======
 import { AuthProvider } from './context/authContext';
 import RegistrarseAlumno from './Componentes/Registros/RegistrarseAlumno';
 import RegistrarsePadres from './Componentes/Registros/RegistrarsePadres';
 import RegistrarsePreceptor from './Componentes/Registros/RegistrasePreceptor';
 import LoginAlumno from './Componentes/InicioSesion/LoginAlumno';
-
+  
 function App(){
   return(
     <AuthProvider>
@@ -25,11 +13,7 @@ function App(){
         <LoginAlumno></LoginAlumno>
       </div>
     </AuthProvider>
->>>>>>> 255db2497b3b31cf7bad748df01f2ecd9f2e21ef
-  )
-import RegistrarseAlumno from './Componentes/Registros/RegistrarseAlumno';
-import RegistrarsePadres from './Componentes/Registros/RegistrarsePadres';
-import RegistrarsePreceptor from './Componentes/Registros/RegistrasePreceptor';
-  }
 
-  
+  )  
+
+  }  
