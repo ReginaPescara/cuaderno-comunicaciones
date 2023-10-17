6566e3b0945fee46b3091658d468f58d59764820
lucascardenas7 <lucas13rojo@gmail.com>
2023-10-17 11:18:28 -0300

    Login de alumno exitoso

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { AuthProvider } from './context/authContext';
 import RegistrarseAlumno from './Componentes/Registros/RegistrarseAlumno';
 import RegistrarsePadres from './Componentes/Registros/RegistrarsePadres';
 import RegistrarsePreceptor from './Componentes/Registros/RegistrasePreceptor';
+import LoginAlumno from './Componentes/InicioSesion/LoginAlumno';
 
 function App(){
   return(
     <AuthProvider>
       <div className="App">
-        <RegistrarseAlumno></RegistrarseAlumno>
+        <LoginAlumno></LoginAlumno>
       </div>
     </AuthProvider>
   )
--- src/Componentes/InicioSesion/LoginAlumno.jsx
+++ src/Componentes/InicioSesion/LoginAlumno.jsx
@@ -14,6 +14,7 @@
 */
 import { useState } from 'react'
 import { Switch } from '@headlessui/react'
+import {useAuth} from "../../context/authContext"
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -21,6 +22,13 @@ function classNames(...classes) {
 
 export default function Example() {
   const [agreed, setAgreed] = useState(false)
+  const auth = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+    auth.login(email, password)
+  }
 
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -37,7 +45,7 @@ export default function Example() {
         />
       </div>
       <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Bienvenido Alumno Registrese!</h2>
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Bienvenido Alumno</h2>
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
@@ -51,6 +59,7 @@ export default function Example() {
             </label>
             <div className="mt-2.5">
               <input
+                onChange={(e)=> setEmail(e.target.value)}
                 placeholder='Example@gmail.com'
                 type="email"
                 name="email"
@@ -69,6 +78,7 @@ export default function Example() {
             </label>
             <div className="mt-2.5">
               <input
+                onChange={(e)=> setPassword(e.target.value)}
                 type="password"
                 name="password"
                 id="password"
@@ -109,11 +119,21 @@ export default function Example() {
         </div>
         <div className="mt-10">
           <button
+            onClick={(e)=> handleLogin(e)}
             type="submit"
             className="block w-full rounded-md bg-gray-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
           >
             Iniciar Sesion
-          </button>
+          </button>   
+        </div>
+
+        <div className="mt-10">
+          <button
+            type="submit"
+            className="block w-full rounded-md bg-gray-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+          >
+            Logout
+          </button>   
         </div>
       </form>
     </div>
