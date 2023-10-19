ed99c03dc448077fb60136d50771c6be6f4948f1
alejo <alejonoviello@gmail.com>
2023-10-19 17:26:17 -0300

    Creando la pantalla principal

--- src/Componentes/home/CardsMenu.jsx
+++ src/Componentes/home/CardsMenu.jsx
@@ -1,49 +1,70 @@
-import * as React from 'react';
-import Box from '@mui/joy/Box';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
-import Typography from '@mui/joy/Typography';
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+*/
+const callouts = [
+  {
+    name: 'Desk and Office',
+    description: 'Work from home accessories',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: '#',
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: '#',
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: '#',
+  },
+]
 
-export default function CardVariants() {
+export default function Example() {
   return (
-    <Box
-      sx={{
-        width: '100%',
-        maxWidth: 500,
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fill, minmax(240px, 1fr))',
-        gap: 2,
-      }}
-    >
-      <Card variant="plain">
-        <CardContent>
-          <Typography level="title-md">Plain card</Typography>
-          <Typography>Description of the card.</Typography>
-        </CardContent>
-      </Card>
+    <div className="bg-gray-100">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+          <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
 
-      <Card variant="outlined">
-        <CardContent>
-          <Typography level="title-md">Outlined card (default)</Typography>
-          <Typography>Description of the card.</Typography>
-        </CardContent>
-      </Card>
-
-      <Card variant="soft">
-        <CardContent>
-          <Typography level="title-md">Soft card</Typography>
-          <Typography>Description of the card.</Typography>
-        </CardContent>
-      </Card>
-
-      <Card variant="solid">
-        <CardContent>
-          <Typography level="title-md" textColor="inherit">
-            Solid card
-          </Typography>
-          <Typography textColor="inherit">Description of the card.</Typography>
-        </CardContent>
-      </Card>
-    </Box>
-  );
+          <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+            {callouts.map((callout) => (
+              <div key={callout.name} className="group relative">
+                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                  <img
+                    src={callout.imageSrc}
+                    alt={callout.imageAlt}
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500">
+                  <a href={callout.href}>
+                    <span className="absolute inset-0" />
+                    {callout.name}
+                  </a>
+                </h3>
+                <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
