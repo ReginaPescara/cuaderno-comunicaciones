de54a6902898cc18f7afe841a340501d5bbdaec1
alejo <alejonoviello@gmail.com>
2023-10-19 17:33:21 -0300

    Mostrando pantalla de Cards Menu

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import RegistrarseAlumno from './Componentes/Registros/RegistrarseAlumno';
 import RegistrarsePadres from './Componentes/Registros/RegistrarsePadres';
 import RegistrarsePreceptor from './Componentes/Registros/RegistrasePreceptor';
 import LoginAlumno from './Componentes/InicioSesion/LoginAlumno';
+import CardsMenu from './Componentes/home/CardsMenu';
   
 function App(){
   return(
     <AuthProvider>
       <div className="App">
-        <LoginAlumno></LoginAlumno>
+        <CardsMenu><LoginAlumno></LoginAlumno></CardsMenu>
       </div>
     </AuthProvider>
   )  
